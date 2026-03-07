7789d13eef6b7f51e201b110e18376d0cc4a43f3
gigayash <yashjainyashjain69@gmail.com>
2026-03-07 22:05:57 +0530

    first code

---
+++ name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main () {
+	system("color F5");
+	cout<< "hello my name yash jain what is your name"<<endl ;
+	string some_one;
+	cin>> some_one;
+	cout<<"good name";
+	getch();
+	return 0 ;
+}
+
