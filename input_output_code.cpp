4b51e71332dbc5efd605c128c56f96bd2185f198
gigayash <yashjainyashjain69@gmail.com>
2026-03-09 14:05:32 +0530

    Added input output code file

---
+++ input_output_code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	system("color F5");
+    int a;
+    cout<< "enter a number ";
+    cin>> a;
+    cout<< "You entered: "<<a;
+    return 0;
+}
