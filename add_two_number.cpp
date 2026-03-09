7c0348dcf0864c0613f63722312d0aa91382df8b
gigayash <yashjainyashjain69@gmail.com>
2026-03-09 17:24:44 +0530

    Added two number add code

---
+++ add_two_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main (){
+	system ("color F7");
+	float num1,num2,sum;
+	cout<<"Enter first number ";
+	cin>> num1;
+	cout<<"Enter another number ";
+	cin>>num2;
+	cout<< "Total sum: ";
+	sum=num1+num2;
+	cout<<sum;
+	getch();
+	return 0;
+	
+}
