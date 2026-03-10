09711151c96b51c55d53a2a97896b6d42a5f9935
yash <yashjainyashjain69@gmail.com>
2026-03-10 22:32:22 +0530

    8 code

--- area_of_rectangle_and_perimeter.cpp
+++ area_of_rectangle_and_perimeter.cpp
@@ -5,13 +5,13 @@ int main()
     system("color F2");
     cout<<"area of rectangle and perimeter\n ";
     double l,w,a,p;
-    cout<<"enter the length ";
+    cout<<"enter the length : ";
     cin>> l;
-    cout<<"enter the width ";
+    cout<<"enter the width : ";
     cin>>w;
     a=l*w;
     p=2*l+2*w;
-    cout<<"area of the rectangle "<<a<<"\n";
-    cout<<"perimeter of the rectangle "<<p;
+    cout<<"area of the rectangle : "<<a<<"\n";
+    cout<<"perimeter of the rectangle : "<<p;
     return 0;
 }
