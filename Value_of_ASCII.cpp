320e640c295577913951eb3231f5d85931fe9bec
gigayash <yashjainyashjain69@gmail.com>
2026-03-10 21:19:23 +0530

    5 code

---
+++ Value_of_ASCII.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    system("color F1");
+char ch;
+cout<<"enter the character " ;
+cin>> ch ;
+cout<< "ASCII value of the "<<ch<<"="<<int(ch);
+return 0 ;
+}
