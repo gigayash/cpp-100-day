78eb4082ad606e874d5af54a50e3396d61cc9ef1
gigayash <yashjainyashjain69@gmail.com>
2026-03-10 21:42:13 +0530

    6 code

---
+++ PRODUCT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    system("color F7");
+    float a,b ;
+    cout <<"two number product"<<endl;
+    cout <<" enter a number ";
+    cin>> a;
+    cout<< "enter another number ";
+    cin>> b;
+    cout<< "product : "<<a*b ;
+    return 0;
+}
