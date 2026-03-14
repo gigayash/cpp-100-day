55a8c09712f4b5b6afd273799e00c6637c072bc8
gigayash <yashjainyashjain69@gmail.com>
2026-03-15 00:02:56 +0530

    10 code

---
+++ fahrenheit_to_celsius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    system("color F2");
+double f,c,b;
+cout<<"code 10: temperature conversion from fahrenheit to celsius"<<endl;
+cout<<"temperature conversion from fahrenheit to celsius"<<endl;
+cout<<"enter the temperature in fahrenheit °F ";
+cin>> f;
+b=f-32;
+c=b*5.0/9.0;
+cout<<"celsius °C"<<"\t"<<c;
+return 0;
+}
